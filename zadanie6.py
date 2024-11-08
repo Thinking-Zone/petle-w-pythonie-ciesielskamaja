e1f7616d5843cd79a5cbaaf27f6a112dfc708b76
ciesielskamaja <majaciesielska405@gmail.com>
2024-11-08 13:19:20 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,2 @@
+#for: Stosowana, gdy znamy liczbę iteracji z góry lub gdy iterujemy po kolekcji. Jest bardziej zwięzła i łatwa w użyciu w takich przypadkach.
+#while: Używana, gdy nie znamy liczby iteracji, ale chcemy powtarzać kod, dopóki spełniony jest warunek. Daje większą elastyczność, ale może być bardziej podatna na błędy związane z niespełnieniem warunku zakończenia.
